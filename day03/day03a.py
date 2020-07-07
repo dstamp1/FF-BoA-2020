9a59fe20888e8a6501ed5f93cca9ee472aafee17
Derek Stampone <dstampone@schools.nyc.gov>
2020-07-07 17:45:15 -0400

    add notes

--- day03/day03a.py
+++
@@ -1,5 +0,0 @@
-Morning Session-- Intro to Functions
-turn off autosave
-
-budget buddy-- git clone https://github.com/upperlinecode/budget-buddy-python-functions.git
-cookie monster -- git clone https://github.com/upperlinecode/cookie-monster-python-functions.git
--- day03/day03b-ExtendedFunctions.py
+++ day03/day03b-ExtendedFunctions.py
@@ -1,5 +1,5 @@
 Mid-Morning Session- Functions Cont'd
-debugging practice 
+debugging practice
 
 investment-- git clone https://github.com/upperlinecode/investment-functions.git
 Dog rater -- git clone https://github.com/upperlinecode/they-rate-dogs-python-functions.git
