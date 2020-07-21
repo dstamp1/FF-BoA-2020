2eeedb5d06e63ecf52c6dd9daa09d6c9df8dd149
Derek Stampone <dstampone@schools.nyc.gov>
2020-07-21 07:56:21 -0400

    Update day 12

--- day12/day12b.py
+++ day12/day12b.py
@@ -2,7 +2,7 @@ Mid-Morning Session
 
 ### How to connect a Flask App to a MongoDB database
 
-## Let's get copies of the community board app we saw this morning
+## Let's get a copy of the community board app we saw this morning
 git clone https://github.com/upperlinecode/stock-query-mongodb.git
 
 ## To connect your database,
@@ -17,7 +17,26 @@ app.config['MONGO_URI'] = f'mongodb+srv://{MONGO_DB_USERNAME}:{MONGO_DB_PASSWORD
 
 mongo = PyMongo(app)
 
+# The proper way to store this information is with an environment variable. To use an environment variable, we need to include sudo pip3 install python-dotenv
 
+sudo pip3 install python-dotenv
+
+#and import the package
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+## and we'll include
+MONGO_DBNAME = os.getenv("MONGO_DBNAME")
+MONGO_DB_USERNAME = os.getenv("MONGO_DB_USERNAME")
+MONGO_DB_PASSWORD = os.getenv("MONGO_DB_PASSWORD")
+
+## So where does the data live? We put it in a file inside the directory called .env This file will not be uploaded into github and the information is kept secret.
+
+# In the .env file, we would put
+MONGO_DBNAME =
+MONGO_DB_USERNAME =
+MONGO_DB_PASSWORD = 
 
 
 ### Now we want to add new documents to our database
@@ -27,3 +46,9 @@ event = {
 'date':''
 }
 events_db.insert(event)
+
+## right now, this route requires us to hard code the event to add it.
+# Let's connect it to the form.
+
+
+## Playtime -- Continue to work on this flask app and add some styling, add additional fields to the form about the event, etc.
