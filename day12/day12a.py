ca9f18bd38f128a60a3d26b8759c6a5cf68ad266
Derek Stampone <dstampone@schools.nyc.gov>
2020-07-20 21:29:57 -0400

    Start writing out day 12

--- day12/day12a.py
+++ day12/day12a.py
@@ -1,1 +1,35 @@
 Morning Session
+
+
+### Morning Routine ###
+
+## Stretch and Share ##
+# Question....if you could be a contestant on any tv show to win a million dollars, which show do you think you'd do the best?
+
+### Buzzfeed Quizzes ###
+
+https://www.buzzfeed.com/
+
+######## Why do we even need a database #######
+
+# Let's say we wanted to build a site where we could post community events and important dates. I want to site to be able to show events + add events.
+# I coded together this app last night: https://community-board-dstamp.herokuapp.com/
+
+## Take 2 minutes to explore my site (it only has two pages) and add an event
+
+
+### At this point, you probabl noticed that the events we added individually aren't showing up on everyone elses page even though we all visited the same website.
+
+## If we look at the code, we see that the original evennts list is hard coded.
+
+### To have a site where multiple users can access and add data for other users to interact with, we need a centralized way to store data. Databases give us the way to do that. We will learn how to user MongoDB.
+
+## This app is just an example of what's possible by incorporating MongoDB. Before we explore how to do that, first let's learn how to interact with an existing MongoDB database.
+
+
+#### Set up for the stocks lab.
+
+## We can think of
+
+## Stocks Lab
+git clone https://github.com/upperlinecode/stock-query-mongodb.git
--- day12/day12b.py
+++ day12/day12b.py
@@ -1,1 +1,3 @@
 Mid-Morning Session
+
+## Sign up for MongoDB + create your first database connection
