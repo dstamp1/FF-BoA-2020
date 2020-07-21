d6bba16504ca954ee726ec3c0b1d6c283a719c63
Derek Stampone <dstampone@schools.nyc.gov>
2020-07-20 22:14:16 -0400

    Continue day 12

--- day12/day12b.py
+++ day12/day12b.py
@@ -1,3 +1,29 @@
 Mid-Morning Session
 
-## Sign up for MongoDB + create your first database connection
+### How to connect a Flask App to a MongoDB database
+
+## Let's get copies of the community board app we saw this morning
+git clone https://github.com/upperlinecode/stock-query-mongodb.git
+
+## To connect your database,
+from flask_pymongo import PyMongo
+
+MONGO_DBNAME = ''
+MONGO_DB_USERNAME = ''
+MONGO_DB_PASSWORD = ''
+
+app.config['MONGO_DBNAME'] = MONGO_DBNAME
+app.config['MONGO_URI'] = f'mongodb+srv://{MONGO_DB_USERNAME}:{MONGO_DB_PASSWORD}@cluster0-kxrbn.mongodb.net/{MONGO_DBNAME}?retryWrites=true'
+
+mongo = PyMongo(app)
+
+
+
+
+### Now we want to add new documents to our database
+events_db = mongo.db.events
+event = {
+'name':'',
+'date':''
+}
+events_db.insert(event)
